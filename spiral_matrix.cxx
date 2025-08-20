7a4f34d376a91869dcf444be41b33fe5e88593e7
kedardole <kedole92@gmail.com>
2025-08-21 00:55:42 +0530

    Create spiral_matrix.cxx

---
+++ spiral_matrix.cxx
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool conditionFail(pair<int, int > &pos, vector<vector<int>> &matrix, int &n){
+    int x = pos.first, y = pos.second;
+    if(x >= n || x < 0 || y >= n || y < 0)
+        return true;
+    if (matrix[x][y] != 0)
+        return true;
+    return false;
+}  
+
+vector<vector<int > > spiralMatrixII(int n){
+    //create a result matrix of n rows and n columns
+    vector<vector<int>> arr(n,vector<int>(n));
+    vector<int> intermediate_matrix;
+
+    int val=1;
+
+    int firstRow=0; int firstCol=0; int lastRow = n-1; int lastCol = n-1;
+
+    for(int col=0; col<=lastCol; col++)
+    {
+        intermediate_matrix.push_back(val);
+    }
+    arr.push_back(intermediate_matrix);
+    
+
+}
+
+int main(){
+    int n;
+    cin>>n;
+    vector<vector<int> > result = spiralMatrixII(n);
+    for(auto &row: result){
+        for(auto &element: row){
+            cout<<element<<" ";
+        }
+        cout<<"\n";
+    } 
+}
