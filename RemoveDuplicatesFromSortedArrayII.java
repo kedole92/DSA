82eb49ac0972cd67bf7b98fefe2335ae6679cc33
kedardole <kedole92@gmail.com>
2025-08-25 21:05:35 +0530

    Update RemoveDuplicatesFromSortedArrayII.java

--- RemoveDuplicatesFromSortedArrayII.java
+++ RemoveDuplicatesFromSortedArrayII.java
@@ -26,5 +26,55 @@ Constraints
 1 <= n <= 10^5
 0 <= nums[i] <= 10^6
 
+class RemoveDuplicatesFromSortedArrayII {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        int res = removeDuplicatesFromSortedArrayII(n, arr);
+
+        System.out.println(res);
+
+        for (int i = 0; i < res; i++)
+            System.out.print(arr[i] + " ");
+
+    }
+
+    static int removeDuplicatesFromSortedArrayII(int n, int[] arr) {
+        int count=0, val =0, j=0;
+
+        //run for loop 
+        for(int i=0; i<n;)
+        {
+            //store current value of arr index in val
+            val = arr[i];
+            count = 0;
+
+            //run a while loop till arr length
+            while(i<n && arr[i] == val)
+            {
+                count++;
+                i++;
+            }
+
+            if(count>2) 
+                count=2;
+
+            while(count-- > 0)
+            {
+                arr[j] = val;
+                j++;
+            }
+        }
+        return j;
+    }
+
+}
+
 
 
