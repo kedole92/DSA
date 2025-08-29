6af98870ebdf32e0275f0d24c1d1c24d18acc730
kedardole <kedole92@gmail.com>
2025-08-29 15:44:52 +0530

    Create coutnsort.java

---
+++ coutnsort.java
@@ -0,0 +1,23 @@
+Problem Description
+A string S is given consisting of lowercase alphabetical characters only. You need to return a sorted string using Count Sort.
+
+Input format
+First line will contain a single integer n representing size of the given string.
+
+Second line will contain a single string S of size n.
+
+Output format
+Output the string in a single line.
+
+Sample Input 1
+10
+
+abcdeedcba
+
+Sample Output 1
+aabbccddee
+
+Constraints
+1<=n<=10^6
+
+String S will contain lowercase alphabetical characters only
